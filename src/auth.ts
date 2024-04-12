22642e6d7b48655c1a18dc1f3ab3175d229b5c33
msga-mmm <michael.sga.mmm@gmail.com>
2024-04-12 00:56:17 -0500

    feat: setup prettier for code formatting

--- src/auth.ts
+++ src/auth.ts
@@ -2,5 +2,5 @@ import NextAuth from "next-auth";
 import Auth0 from "next-auth/providers/auth0";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-	providers: [Auth0],
+  providers: [Auth0],
 });
--- src/components/sign-in.tsx
+++ src/components/sign-in.tsx
@@ -1,14 +1,14 @@
 import { signIn } from "@/auth";
 
 export function SignIn() {
-	return (
-		<form
-			action={async () => {
-				"use server";
-				await signIn("auth0");
-			}}
-		>
-			<button type="submit">Signin with Auth0</button>
-		</form>
-	);
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("auth0");
+      }}
+    >
+      <button type="submit">Signin with Auth0</button>
+    </form>
+  );
 }
